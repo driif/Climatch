64dac43afad8950183ac6a7c3f5fee68db59b933
Andrii Fedorchuk <mednessnews@gmail.com>
2022-08-19 18:54:10 +0200

    Changes for Production Build

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,7 +15,7 @@ import { join } from 'path';
       isGlobal: true,
     }),
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', '../dist/frontend'),
+      rootPath: join(__dirname, '..', 'dist/public'),
     }),
     UserModule,
     AuthModule,
