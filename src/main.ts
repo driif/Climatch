eb7f738351424588e82cc2fa8789f9f778f304a8
Andrii Fedorchuk <mednessnews@gmail.com>
2022-08-19 22:33:55 +0200

    Changes for Production Build

--- src/main.ts
+++ src/main.ts
@@ -4,30 +4,15 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  const whitelist = [
-    'http://localhost:3000',
-    'localhost:4200',
-    'https://climatch.herokuapp.com',
-    'climatch-api.herokuapp.com',
-    'https://climatch.herokuapp.com/',
-    '0.0.0.0:3000',
-    '127.0.0.1:3000',
-  ];
-  app.enableCors({
-    origin: function (origin, callback) {
-      if (whitelist.indexOf(origin) !== -1) {
-        console.log('allowed cors for:', origin);
-        callback(null, true);
-      } else {
-        console.log('blocked cors for:', origin);
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    allowedHeaders:
-      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
-    methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
-    credentials: true,
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
+  app.enableCors();
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
   });
   app.useGlobalPipes(
     new ValidationPipe({
