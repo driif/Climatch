568b84e9e71f0d6fe37b27e0024016518ac8d909
Andrii Fedorchuk <mednessnews@gmail.com>
2022-08-19 21:20:23 +0200

    Changes for Production Build

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['localhost:4200'],
+    origin: ['localhost:3000', 'localhost:4200'],
     methods: ['GET', 'PUT', 'POST', 'DELETE'],
     credentials: true,
   });
