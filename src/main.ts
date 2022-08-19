503c41d9e579eaae315dab872cc88a279f16ae5d
Andrii Fedorchuk <mednessnews@gmail.com>
2022-08-19 21:57:44 +0200

    Changes for Production Build

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const whitelist = [
+    'http://localhost:3000',
+    'localhost:4200',
+    'https://climatch.herokuapp.com',
+    'climatch-api.herokuapp.com',
+    'https://climatch.herokuapp.com/',
+    '0.0.0.0:3000',
+    '127.0.0.1:3000',
+  ];
   app.enableCors({
-    origin: ['*'],
-    methods: ['GET', 'PUT', 'POST', 'DELETE'],
+    origin: function (origin, callback) {
+      if (whitelist.indexOf(origin) !== -1) {
+        console.log('allowed cors for:', origin);
+        callback(null, true);
+      } else {
+        console.log('blocked cors for:', origin);
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    allowedHeaders:
+      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+    methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
     credentials: true,
   });
   app.useGlobalPipes(
