97e7dfa9bd7cbce23ec544819a7ee2900d04cbde
Andrii Fedorchuk <mednessnews@gmail.com>
2022-08-19 21:43:23 +0200

    Changes for Production Build

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [
-      'localhost:3000',
-      'localhost:4200',
-      'http://climatch.herokuapp.com',
-      'climatch-api.herokuapp.com',
-      'http://climatch.herokuapp.com/',
-      '0.0.0.0:3000',
-      '127.0.0.1:3000',
-    ],
+    origin: ['*'],
     methods: ['GET', 'PUT', 'POST', 'DELETE'],
     credentials: true,
   });
